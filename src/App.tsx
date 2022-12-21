f8bb5709f38abda6991abf41f82e0a20e698aa4c
saber <1558365539@qq.com>
2022-12-21 11:50:41 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { DatePicker, Button, Space } from 'antd'
+const App: React.FC = () => {
+	const [count, setCount] = useState(0)
+
+	return (
+		<div className='App'>
+			<div>
+				<a href='https://vitejs.dev' target='_blank'>
+					<img src='/vite.svg' className='logo' alt='Vite logo' />
+				</a>
+				<a href='https://reactjs.org' target='_blank'>
+					<img src={reactLogo} className='logo' alt='React logo' />
+				</a>
+			</div>
+			<h1>Vite + React</h1>
+			<div className='card'>
+				<Space wrap>
+					<Button type='primary' onClick={() => setCount((count) => count + 1)}>
+						count is {count}
+					</Button>
+					<Button type='primary'>Primary Button</Button>
+					<Button>Default Button</Button>
+					<Button type='dashed'>Dashed Button</Button>
+					<Button type='text'>Text Button</Button>
+					<Button type='link'>Link Button</Button>
+				</Space>
+			</div>
+			<p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+			<DatePicker />
+			<video src='/assets/ant.mp4' autoPlay loop muted />
+		</div>
+	)
+}
+
+export default App
